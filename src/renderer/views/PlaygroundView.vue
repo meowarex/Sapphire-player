7feb14f61ba00a89a288050b91e70217f5e92f8f
Atomix <90243579+A-T-O-M-I-X@users.noreply.github.com>
2023-01-12 00:47:52 +1000

    Add files via upload

---
+++ src/renderer/views/PlaygroundView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="text-12px p-2 w-full borderRight">
+    <header class="italic opacity-50 text-primary-1000">
+      This is the playground area, use this view to test out and develop components
+    </header>
+  </div>
+</template>
+
+<style scoped lang="postcss">
+
+</style>
